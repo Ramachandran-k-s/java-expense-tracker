103a9c60e0da9b04aa6161c2b995da804b5cd5f5
Ramachandran-k-s <ramachandranks2006@gmail.com>
2026-06-07 22:09:56 +0530

    Added basic expense tracker

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Expense Tracker Started!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Expense Tracker");
+
+        System.out.print("Enter income: ");
+        double income = sc.nextDouble();
+
+        System.out.print("Enter expense: ");
+        double expense = sc.nextDouble();
+
+        double balance = income - expense;
+
+        System.out.println("Balance = " + balance);
     }
 }
